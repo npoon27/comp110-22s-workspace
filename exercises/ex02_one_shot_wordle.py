8a5f431f8b2b62b9f746fe3d614e865917b6c295
Nicholas Poon <npoon@unc.edu>
2022-01-27 09:35:24 -0500

    Finished Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -10,23 +10,23 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(user_guess) != len(secret_word):
-    user_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+    user_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
 index_variable: int = 0
 emojis_of_guess: str = ""
 while index_variable < len(secret_word):
     if user_guess[index_variable] == secret_word[index_variable]:
-        emojis_of_guess = emojis_of_guess + GREEN_BOX + " "
+        emojis_of_guess = emojis_of_guess + GREEN_BOX
     else:
         yellow_box_variable: int = 0
         while yellow_box_variable < len(secret_word):
             if user_guess[index_variable] == secret_word[yellow_box_variable]:
-                emojis_of_guess = emojis_of_guess + YELLOW_BOX + " "
+                emojis_of_guess = emojis_of_guess + YELLOW_BOX
                 yellow_box_variable = yellow_box_variable + len(secret_word) + 1
             else:
                 yellow_box_variable = yellow_box_variable + 1
         if yellow_box_variable == len(secret_word):
-            emojis_of_guess = emojis_of_guess + WHITE_BOX + " "
+            emojis_of_guess = emojis_of_guess + WHITE_BOX
     index_variable = index_variable + 1
 
 if user_guess == secret_word:
