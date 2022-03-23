48d19f05acb166fe97f643ef5779a5fa53ba1392
Nicholas Poon <npoon@unc.edu>
2022-03-22 20:25:28 -0400

    Finished Exercise 7

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -37,3 +37,49 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     for column in first_row:
         result[column] = column_values(row_table, column)
     return result
+
+
+def head(column_table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Create a comlumn-based table with the first n given rows of data."""
+    result: dict[str, list[str]] = {}
+    for columns in column_table:
+        row_values: list[str] = []
+        i: int = 0
+        while i < n:
+            row_values.append(column_table[columns][n])
+            i += 1
+        result[columns] = row_values
+    return result
+
+
+def select(given_table: dict[str, list[str]], wanted_columns: list[str]) -> dict[str, list[str]]:
+    """Create a new column-based table with a specific subset of columns from given table."""
+    result: dict[str, list[str]] = {}
+    for columns in wanted_columns:
+        if columns in given_table:
+            result[columns] = given_table[columns]
+    return result
+
+
+def concat(table_one: dict[str, list[str]], table_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Crate a new column-based table by combining two column-based tables."""
+    result: dict[str, list[str]] = {}
+    for columns in table_one:
+        result[columns] = table_one[columns]
+    for columns in table_two:
+        if columns in result:
+            result[columns] += table_two[columns]
+        else:
+            result[columns] = table_two[columns]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Counts how many times a value appears in a list."""
+    result: dict[str, int] = {}
+    for x in values:
+        if x in result:
+            result[x] += 1
+        else:
+            result[x] = 1
+    return result
